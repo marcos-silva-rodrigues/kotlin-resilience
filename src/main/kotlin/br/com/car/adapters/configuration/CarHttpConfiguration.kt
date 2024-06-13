750b85123eaf8b89edf91a08b4671307e7d43d64
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-06-13 10:33:54 -0300

    feat: add circuit breaker in retrofit

--- src/main/kotlin/br/com/car/adapters/configuration/CarHttpConfiguration.kt
+++ src/main/kotlin/br/com/car/adapters/configuration/CarHttpConfiguration.kt
@@ -1,6 +1,8 @@
 package br.com.car.adapters.configuration
 
 import br.com.car.adapters.http.CarHttpService
+import io.github.resilience4j.circuitbreaker.CircuitBreaker
+import io.github.resilience4j.retrofit.CircuitBreakerCallAdapter
 import okhttp3.OkHttp
 import okhttp3.OkHttpClient
 import org.springframework.context.annotation.Bean
@@ -9,7 +11,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 @Configuration
-class CarHttpConfiguration {
+class CarHttpConfiguration(
+    private val circuitBreaker: CircuitBreaker
+) {
 
     private companion object {
         const val BASE_URL = "http://ec2-18-215-156-13.compute-1.amazonaws.com:8080"
@@ -18,11 +22,14 @@ class CarHttpConfiguration {
     fun okHttpClient() = OkHttpClient.Builder().build()
 
     fun buildRetrofit() = Retrofit.Builder()
+        .addCallAdapterFactory(CircuitBreakerCallAdapter
+            .of(circuitBreaker))
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .client(okHttpClient())
         .build()
 
     @Bean
-    fun carHttpService(): CarHttpService = buildRetrofit().create(CarHttpService::class.java)
+    fun carHttpService(): CarHttpService = buildRetrofit()
+        .create(CarHttpService::class.java)
 }
